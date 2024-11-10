253350e286297b7e26a42b4628930fc7da57fea7
bestorbust <priyadarshinip658@gmail.com>
2024-11-10 22:04:33 +0530

    scheduling

--- scheduling.c
+++ scheduling.c
@@ -8,14 +8,13 @@ void priority_non_preemptive();
 void priority_preemptive();
 void round_robin();
 
-// Main function
 int main() {
     int choice;
     do {
         printf("\nCPU Scheduling Simulator\n");
         printf("1. First-Come, First-Served (FCFS)\n");
         printf("2. Shortest Job First (SJF) - Non-Preemptive\n");
-        printf("3. Shortest Job First (SJF) - Preemptive\n");
+        printf("3. Shortest Job First (SRTF) - Preemptive\n");
         printf("4. Priority Scheduling - Non-Preemptive\n");
         printf("5. Priority Scheduling - Preemptive\n");
         printf("6. Round Robin (RR)\n");
@@ -53,304 +52,358 @@ int main() {
     return 0;
 }
 
-// FCFS Implementation
+// First-Come, First-Served (FCFS)
 void fcfs() {
     int n, i;
-    int bt[20], wt[20], tat[20];
+    int bt[20], at[20], wt[20], tat[20], ct[20];
     float avg_wt = 0, avg_tat = 0;
 
     printf("\n--- First-Come, First-Served (FCFS) Scheduling ---\n");
     printf("Enter number of processes: ");
     scanf("%d", &n);
-    printf("Enter burst time for each process:\n");
+
+    printf("Enter arrival time and burst time for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("P[%d]: ", i + 1);
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
         scanf("%d", &bt[i]);
     }
 
-    // Waiting time calculation
-    wt[0] = 0;
+    // Calculate completion time
+    ct[0] = at[0] + bt[0];
     for(i = 1; i < n; i++) {
-        wt[i] = wt[i - 1] + bt[i - 1];
+        if (at[i] > ct[i - 1])
+            ct[i] = at[i] + bt[i];
+        else
+            ct[i] = ct[i - 1] + bt[i];
     }
 
-    // Turnaround time calculation
+    // Calculate waiting and turnaround times
     for(i = 0; i < n; i++) {
-        tat[i] = wt[i] + bt[i];
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
         avg_wt += wt[i];
         avg_tat += tat[i];
     }
+    
+   
 
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for(i = 0; i < n; i++) {
-        printf("P[%d]\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
     printf("\nAverage Waiting Time: %.2f", avg_wt/n);
     printf("\nAverage Turnaround Time: %.2f\n", avg_tat/n);
+    
+
 }
 
-// SJF Non-Preemptive Implementation
+// Shortest Job First (Non-Preemptive)
 void sjf_non_preemptive() {
-    int n, bt[20], p[20], wt[20], tat[20], i, j, pos, temp;
+    int n, i, j, min_index, time = 0;
+    int at[20], bt[20], ct[20], tat[20], wt[20], completed[20] = {0};
     float total_wt = 0, total_tat = 0;
 
-    printf("\n--- Shortest Job First (Non-Preemptive) Scheduling ---\n");
+    printf("\n--- Shortest Job First (Non-Preemptive) ---\n");
     printf("Enter number of processes: ");
     scanf("%d", &n);
-    printf("Enter burst time for each process:\n");
+
+    printf("Enter arrival time and burst time for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("P[%d]: ", i + 1);
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
         scanf("%d", &bt[i]);
-        p[i] = i + 1;
     }
 
-    // Sorting burst time in ascending order using selection sort
-    for(i = 0; i < n; i++) {
-        pos = i;
-        for(j = i + 1; j < n; j++) {
-            if(bt[j] < bt[pos])
-                pos = j;
+    int completed_processes = 0;
+    while (completed_processes < n) {
+        min_index = -1;
+
+        // Find the process with the shortest burst time that has arrived and is not yet completed
+        for (j = 0; j < n; j++) {
+            if (completed[j] == 0 && at[j] <= time) {
+                if (min_index == -1 || bt[j] < bt[min_index]) {
+                    min_index = j;
+                }
+            }
         }
 
-        temp = bt[i];
-        bt[i] = bt[pos];
-        bt[pos] = temp;
+        if (min_index == -1) {
+            time++;
+            continue;
+        }
 
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
+        // Process the selected job
+        time += bt[min_index];
+        ct[min_index] = time;
+        tat[min_index] = ct[min_index] - at[min_index];
+        wt[min_index] = tat[min_index] - bt[min_index];
+        
+        total_wt += wt[min_index];
+        total_tat += tat[min_index];
+
+        completed[min_index] = 1;
+        completed_processes++;
     }
 
-    wt[0] = 0;  // Waiting time for first process is 0
-
-    // Waiting time calculation
-    for(i = 1; i < n; i++) {
-        wt[i] = 0;
-        for(j = 0; j < i; j++)
-            wt[i] += bt[j];
-        total_wt += wt[i];
-    }
-
-    // Turnaround time calculation
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for(i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
-        total_tat += tat[i];
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
-
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for(i = 0; i < n; i++) {
-        printf("P[%d]\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
-    }
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
     printf("\nAverage Waiting Time: %.2f", total_wt/n);
     printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
 }
-// SJF Preemptive (Shortest Remaining Time First) Implementation
+
+
+// Shortest Job First (Preemptive)
 void sjf_preemptive() {
-    int arrival_time[10], burst_time[10], temp[10];
-    int i, smallest, count = 0, time, n;
-    double wait_time = 0, turnaround_time = 0, end;
-    float average_waiting_time, average_turnaround_time;
+    int n, i, j, time = 0, smallest, count = 0, end;
+    int at[20], bt[20], remaining_bt[20], ct[20], tat[20], wt[20];
+    float total_wt = 0, total_tat = 0;
 
     printf("\n--- Shortest Job First (Preemptive) Scheduling ---\n");
-    printf("Enter number of Processes: ");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
+    printf("Enter arrival time and burst time for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("Enter arrival time for Process P[%d]: ", i + 1);
-        scanf("%d", &arrival_time[i]);
-        printf("Enter burst time for Process P[%d]: ", i + 1);
-        scanf("%d", &burst_time[i]);
-        temp[i] = burst_time[i];
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+        remaining_bt[i] = bt[i];
     }
 
-    burst_time[9] = 9999;
+    remaining_bt[19] = 9999; // large value for comparison
     for(time = 0; count != n; time++) {
-        smallest = 9;
+        smallest = 19;
         for(i = 0; i < n; i++) {
-            if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+            if(at[i] <= time && remaining_bt[i] < remaining_bt[smallest] && remaining_bt[i] > 0) {
                 smallest = i;
+            }
         }
-        burst_time[smallest]--;
-
-        if(burst_time[smallest] == 0) {
+        remaining_bt[smallest]--;
+        if(remaining_bt[smallest] == 0) {
             count++;
             end = time + 1;
-            wait_time += end - arrival_time[smallest] - temp[smallest];
-            turnaround_time += end - arrival_time[smallest];
+            ct[smallest] = end;
+            tat[smallest] = ct[smallest] - at[smallest];
+            wt[smallest] = tat[smallest] - bt[smallest];
+            total_wt += wt[smallest];
+            total_tat += tat[smallest];
         }
     }
 
-    average_waiting_time = wait_time / n;
-    average_turnaround_time = turnaround_time / n;
-
-    printf("\nAverage Waiting Time: %.2f", average_waiting_time);
-    printf("\nAverage Turnaround Time: %.2f\n", average_turnaround_time);
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for(i = 0; i < n; i++) {
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
+    printf("\nAverage Waiting Time: %.2f", total_wt/n);
+    printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
 }
 
-// Priority Non-Preemptive Implementation
+// Priority Scheduling (Non-Preemptive)
 void priority_non_preemptive() {
-    int bt[20], p[20], wt[20], tat[20], pr[20], i, j, n, pos, temp;
+    int n, i, j, max_index, time = 0;
+    int at[20], bt[20], priority[20], ct[20], wt[20], tat[20], completed[20] = {0};
     float total_wt = 0, total_tat = 0;
 
     printf("\n--- Priority Scheduling (Non-Preemptive) ---\n");
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
+    printf("Enter arrival time, burst time, and priority for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("\nEnter burst time for Process P[%d]: ", i + 1);
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
         scanf("%d", &bt[i]);
-        printf("Enter priority for Process P[%d]: ", i + 1);
-        scanf("%d", &pr[i]);
-        p[i] = i + 1;
+        printf("Priority: ");
+        scanf("%d", &priority[i]);
     }
 
-    // Sorting based on priority
-    for(i = 0; i < n; i++) {
-        pos = i;
-        for(j = i + 1; j < n; j++) {
-            if(pr[j] < pr[pos])
-                pos = j;
+    int completed_processes = 0;
+    time = 0;
+
+    while (completed_processes < n) {
+        max_index = -1;
+
+        // Find the highest priority process that has arrived and is not completed
+        for (j = 0; j < n; j++) {
+            if (completed[j] == 0 && at[j] <= time) {
+                if (max_index == -1 || priority[j] > priority[max_index]) {  // Reverse comparison
+                    max_index = j;
+                }
+            }
         }
 
-        temp = pr[i];
-        pr[i] = pr[pos];
-        pr[pos] = temp;
+        // If no process is ready, increment time to next arrival
+        if (max_index == -1) {
+            time++;
+            continue;
+        }
 
-        temp = bt[i];
-        bt[i] = bt[pos];
-        bt[pos] = temp;
+        // Execute the selected process
+        time += bt[max_index];
+        ct[max_index] = time;
+        tat[max_index] = ct[max_index] - at[max_index];
+        wt[max_index] = tat[max_index] - bt[max_index];
 
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
+        total_wt += wt[max_index];
+        total_tat += tat[max_index];
+
+        completed[max_index] = 1;
+        completed_processes++;
     }
 
-    wt[0] = 0;
-    for(i = 1; i < n; i++) {
-        wt[i] = 0;
-        for(j = 0; j < i; j++)
-            wt[i] += bt[j];
-        total_wt += wt[i];
-    }
-
+    printf("\nProcess\tArrival Time\tBurst Time\tPriority\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for(i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
-        total_tat += tat[i];
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], priority[i], ct[i], tat[i], wt[i]);
     }
-
-    printf("\nProcess\tBurst Time\tPriority\tWaiting Time\tTurnaround Time");
-    for(i = 0; i < n; i++)
-        printf("\nP[%d]\t%d\t\t%d\t\t%d\t\t%d", p[i], bt[i], pr[i], wt[i], tat[i]);
-
-    printf("\n\nAverage Waiting Time: %.2f", total_wt/n);
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
+    printf("\nAverage Waiting Time: %.2f", total_wt/n);
     printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
 }
 
-// Priority Preemptive Implementation
+
+// Priority Scheduling (Preemptive)
 void priority_preemptive() {
-    int bt[20], p[20], wt[20], tat[20], pr[20], rt[20], i, j, n, time = 0, count = 0, smallest;
+    int n, i, time, largest, remain = 0, end;
+    int at[20], bt[20], remaining_bt[20], priority[20], ct[20], wt[20], tat[20];
     float total_wt = 0, total_tat = 0;
 
     printf("\n--- Priority Scheduling (Preemptive) ---\n");
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
+    printf("Enter arrival time, burst time, and priority for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("\nEnter burst time for Process P[%d]: ", i + 1);
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
         scanf("%d", &bt[i]);
-        printf("Enter priority for Process P[%d]: ", i + 1);
-        scanf("%d", &pr[i]);
-        p[i] = i + 1;
-        rt[i] = bt[i];
+        printf("Priority: ");
+        scanf("%d", &priority[i]);
+        remaining_bt[i] = bt[i];  // Initialize remaining burst time
     }
 
-    while(count != n) {
-        smallest = -1;
-        int highest_priority = 99999;
+    priority[19] = 9999; // Dummy priority for comparison
+    for(time = 0; remain != n; time++) {
+        largest = -1;
 
+        // Find the process with the highest priority (largest value) that has arrived and has remaining burst time
         for(i = 0; i < n; i++) {
-            if(rt[i] > 0 && pr[i] < highest_priority) {
-                highest_priority = pr[i];
-                smallest = i;
+            if(at[i] <= time && remaining_bt[i] > 0) {
+                if (largest == -1 || priority[i] > priority[largest]) {  // Reverse comparison
+                    largest = i;
+                }
             }
         }
 
-        if(smallest == -1)
-            break;
+        // If no process is ready, skip this time unit
+        if(largest == -1) {
+            continue;
+        }
 
-        rt[smallest]--;
-        time++;
+        remaining_bt[largest]--; // Process the selected job for 1 time unit
 
-        if(rt[smallest] == 0) {
-            count++;
-            tat[smallest] = time;
-            wt[smallest] = time - bt[smallest];
+        // If process is finished
+        if(remaining_bt[largest] == 0) {
+            remain++;
+            end = time + 1;
+            ct[largest] = end;
+            tat[largest] = ct[largest] - at[largest];
+            wt[largest] = tat[largest] - bt[largest];
+            total_wt += wt[largest];
+            total_tat += tat[largest];
         }
     }
 
+    printf("\nProcess\tArrival Time\tBurst Time\tPriority\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for(i = 0; i < n; i++) {
-        total_wt += wt[i];
-        total_tat += tat[i];
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], priority[i], ct[i], tat[i], wt[i]);
     }
-
-    printf("\nProcess\tBurst Time\tPriority\tWaiting Time\tTurnaround Time");
-    for(i = 0; i < n; i++)
-        printf("\nP[%d]\t%d\t\t%d\t\t%d\t\t%d", p[i], bt[i], pr[i], wt[i], tat[i]);
-
-    printf("\n\nAverage Waiting Time: %.2f", total_wt/n);
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
+    printf("\nAverage Waiting Time: %.2f", total_wt/n);
     printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
 }
 
-// Round Robin Implementation
+
+// Round Robin Scheduling
 void round_robin() {
-    int i, j, n, time, remain, flag = 0, time_quantum;
-    int wait_time = 0, turnaround_time = 0, at[10], bt[10], rt[10];
+    int i, j, n, tq, time = 0, remain, flag = 0;
+    int at[10], bt[10], rt[10], wt[10], tat[10], ct[10];
+    float total_wt = 0, total_tat = 0;
 
     printf("\n--- Round Robin (RR) Scheduling ---\n");
     printf("Enter number of processes: ");
     scanf("%d", &n);
     remain = n;
 
+    printf("Enter arrival time and burst time for each process:\n");
     for(i = 0; i < n; i++) {
-        printf("Enter arrival time for Process P[%d]: ", i + 1);
+        printf("Process P[%d]:\n", i + 1);
+        printf("Arrival Time: ");
         scanf("%d", &at[i]);
-        printf("Enter burst time for Process P[%d]: ", i + 1);
+        printf("Burst Time: ");
         scanf("%d", &bt[i]);
         rt[i] = bt[i];
     }
 
     printf("Enter time quantum: ");
-    scanf("%d", &time_quantum);
-
-    printf("\nProcess\t|Turnaround Time|Waiting Time\n");
+    scanf("%d", &tq);
 
     for(time = 0, i = 0; remain != 0;) {
-        if(rt[i] <= time_quantum && rt[i] > 0) {
+        if(rt[i] <= tq && rt[i] > 0) {
             time += rt[i];
             rt[i] = 0;
             flag = 1;
+        } else if(rt[i] > 0) {
+            rt[i] -= tq;
+            time += tq;
         }
-        else if(rt[i] > 0) {
-            rt[i] -= time_quantum;
-            time += time_quantum;
-        }
-
         if(rt[i] == 0 && flag == 1) {
             remain--;
-            printf("P[%d]\t|\t%d\t|\t%d\n", i + 1, time - at[i], time - at[i] - bt[i]);
-            wait_time += time - at[i] - bt[i];
-            turnaround_time += time - at[i];
+            ct[i] = time;
+            tat[i] = time - at[i];
+            wt[i] = tat[i] - bt[i];
+            total_wt += wt[i];
+            total_tat += tat[i];
             flag = 0;
         }
-
-        if(i == n - 1)
-            i = 0;
-        else if(at[i + 1] <= time)
-            i++;
-        else
-            i = 0;
+        if(i == n - 1) i = 0;
+        else if(at[i + 1] <= time) i++;
+        else i = 0;
     }
 
-    printf("\nAverage Waiting Time: %.2f", (float)wait_time/n);
-    printf("\nAverage Turnaround Time: %.2f\n", (float)turnaround_time/n);
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for(i = 0; i < n; i++) {
+        printf("P[%d]\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+    
+    float throughput = (float)n / (float)(tat[n - 1]);  
+    printf("\nThroughput: %.2f processes per unit time\n", throughput);
+    printf("\nAverage Waiting Time: %.2f", total_wt/n);
+    printf("\nAverage Turnaround Time: %.2f\n", total_tat/n);
 }
